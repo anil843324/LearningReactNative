d99c78155254e7b90b441717bc64ec31dc85d11e
anil843324 <anilkrsingh14200@gmail.com>
2023-01-11 23:08:05 +0530

    almost completed calculator

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,117 +1,136 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import {View, Text, Switch, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [result, setResult] = useState('asdf');
+  const colors = {
+    dark: '#22252D',
+    dark1: '#292836',
+    dark2: '#272B33',
+    light: '#FFF',
+    light1: '#F1F1F1',
+    light2: '#F7F7F7',
+  };
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const calculate=(title)=>{
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+         if(title=='C'){
+                 setResult('');
+         }else if(title=='DL'){
+               setResult( result.substring(0, result.length-1))
+         }else if(title=='='){
+           const ans= Number(eval(result).toFixed(3)).toString()
+          setResult( ans)
+                 
+         }else setResult( result+title)
+
+  }
+
+  const Btn = ({title,type}) => {
+    return (
+      <TouchableOpacity
+       onPress={ () => calculate(title)}
+        style={{
+          padding: 10,
+
+          backgroundColor: getColor(colors.light1, colors.dark2),
+          height: 70,
+          width: 70,
+          margin: 10,
+          borderRadius: 10,
+          elevation: 2,
+        }}>
+        <Text
+          style={{
+            fontSize: 37,
+            color: 'black',
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            color:getBtnColor(type)
+          }}>
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+  const  getBtnColor=(type)=> {
+         if(type=='top'){
+                 return '#35FBD6'
+         }else if(type=='right'){
+          return '#EB6363'
+         }else{
+          return getColor(colors.dark,colors.light)
+         }
+
+  }
+
+  const getColor = (light, dark) => (darkTheme ? dark : light);
+
   return (
-    <View style={styles.sectionContainer}>
+    <View
+      style={{
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+        paddingVertical: 20,
+        backgroundColor: getColor(colors.light, colors.dark),
+      }}>
+      <Switch
+        value={darkTheme}
+        thumbColor={getColor(colors.dark, colors.light)}
+        trackColor={{true: colors.light2, false: colors.dark2}}
+        onValueChange={() => setDarkTheme(!darkTheme)}
+      />
       <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+        style={{
+          fontSize: 40,
+          color: getColor(colors.dark, colors.light),
+          width: '100%',
+          textAlign: 'right',
+          paddingRight: 20,
+          marginTop:160,
+          paddingBottom:90
+
+        }}>
+        {result}
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          backgroundColor: getColor(colors.light1, colors.dark1),
+          borderTopRightRadius: 20,
+          borderTopLeftRadius: 20,
+        }}>
+        <Btn title="C" type="top" />
+        <Btn title="DL" type="top" />
+        <Btn title="/" type="top" />
+        <Btn title="%" type="top" />
+
+        <Btn title="7" type='number' />
+        <Btn title="8" type='number' />
+        <Btn title="9" type='number' />
+        <Btn title="*" type='right' />
+
+        <Btn title="4" type='number' />
+        <Btn title="5" type='number' />
+        <Btn title="6" type='number' />
+        <Btn title="-"  type='right' />
+
+        <Btn title="1" type='number' />
+        <Btn title="2" type='number' />
+        <Btn title="3" type='number' />
+        <Btn title="+" type='right' />
+
+        <Btn title="00" type='number' />
+        <Btn title="0" type='number' />
+        <Btn title="." type='number' />
+        <Btn title="=" type='right' />
+      </View>
     </View>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- AwesomeProject/android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ AwesomeProject/android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,5 +1,5 @@
 package com.awesomeproject;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -9,7 +9,17 @@ public class MainActivity extends ReactActivity {
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
+
    */
+
+    //react-native-screens override
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
+
+
+
   @Override
   protected String getMainComponentName() {
     return "AwesomeProject";
