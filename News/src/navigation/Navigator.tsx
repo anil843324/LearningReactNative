8dc8785f9df1fd50e8156967f96a9971541869b4
anil843324 <anilkrsingh14200@gmail.com>
2023-02-26 17:31:32 +0530

    tab navigation created

--- News/src/navigation/Navigator.tsx
+++ News/src/navigation/Navigator.tsx
@@ -1,16 +1,57 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../components/Home';
-import NewsOverview from '../components/NewsOverview';
 
+import NewsOverview from '../components/NewsOverview';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+import Saved from '../screens/Saved';
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
+const HomeScreen = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: true,
+          tabBarIcon: ({color, size}) => (
+            <Image
+              source={require('../../icons/home.png')}
+              style={{height: 24, width: 24}}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Saved"
+        component={Saved}
+        options={{
+          headerShown: true,
+          tabBarIcon: ({color, size}) => (
+            <Image
+              source={require('../../icons/saved.png')}
+              style={{height: 24, width: 24}}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
         <Stack.Screen name="NewsOverview" component={NewsOverview} />
       </Stack.Navigator>
     </NavigationContainer>
--- News/src/screens/Home.tsx
+++ News/src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const Home = () => {
@@ -10,3 +10,5 @@ const Home = () => {
 };
 
 export default Home;
+
+const styles = StyleSheet.create({});
